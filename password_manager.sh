91f42aa61bd418182f24810d570b030c695e8405
kabasunny <dxbqd553@yahoo.co.jp>
2024-05-24 11:24:46 +0900

    step2用件の一部を実装

--- password_manager.sh
+++ password_manager.sh
@@ -2,11 +2,35 @@
 
 echo "パスワードマネージャへようこそ！"
 
-read -p "サービス名を入力してください:" service;
-read -p "ユーザー名を入力してください:" user;
-read -p "パスワード名を入力してください:" password;
+while true
+do
+	read -p "次の選択肢から入力してください(Add Password/Get Password/Exit)：" input
 
-echo "$service:$user:$password" >> pswlog.txt
+       if [ "$input" = "Add Password" ];
+       then  
+	       read -p "サービス名を入力してください:" service;
+	       read -p "ユーザー名を入力してください:" user;
+	       read -p "パスワード名を入力してください:" password;
+	       echo "$service:$user:$password" >> pswlog.txt
+	       echo "パスワードの追加は成功しました。"
+   
+       elif [ "$input" = "Get Password" ];
+       then
+	       read -p "サービス名を入力してください:" service;
+
+       
+
+       
+       elif [ "$input" = "Exit" ];
+       then
+	       break
+       
+       else
+	       echo "入力が間違えています。Add Password/Get Password/Exit から入力してください。"
+
+       fi
+
+done
 
 
 echo "Thank you!";
