a83d8bb0e04ad92e945c10d258ebd458ea6f74ab
kabasunny <dxbqd553@yahoo.co.jp>
2024-05-24 16:07:05 +0900

    step2の要件網羅

--- password_manager.sh
+++ password_manager.sh
@@ -8,19 +8,26 @@ do
 
        if [ "$input" = "Add Password" ];
        then  
-	       read -p "サービス名を入力してください:" service;
-	       read -p "ユーザー名を入力してください:" user;
-	       read -p "パスワード名を入力してください:" password;
+	       read -p "サービス名を入力してください:" service
+	       read -p "ユーザー名を入力してください:" user
+	       read -p "パスワード名を入力してください:" password
 	       echo "$service:$user:$password" >> pswlog.txt
 	       echo "パスワードの追加は成功しました。"
    
        elif [ "$input" = "Get Password" ];
        then
-	       read -p "サービス名を入力してください:" service;
+	       read -p "サービス名を入力してください:" service_2
+	       if grep -q "^$service_2:" pswlog.txt;
+	       then
+		       user_2=$(grep "^$service_2:" pswlog.txt | cut -d':' -f2)
+		       password_2=$(grep "^$service_2:" pswlog.txt | cut -d':' -f3)
+		       echo "ユーザー名: $user_2"
+		       echo "パスワード: $password_2"
 
-       
+	       else
+		       echo "そのサービスは登録されていません。"
 
-       
+	       fi
        elif [ "$input" = "Exit" ];
        then
 	       break
